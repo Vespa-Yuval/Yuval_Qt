afbabf7cf727e8090b60aa58c0565937df1a5984
Vespa-Yuval <yuvalsherer@gmail.com>
2025-06-05 13:29:00 +0300

    Add files via upload
    עדכנתי את המחלקה של המדף  
     זה יצר קונפליקט עם המחלקה של  הספר ולכן העברתי להערה

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 # המחלקה הגבוהה ביותר שלוקחת נתונים מכל המחלקות שמתחת
-class Library (shelve):
-    def __int__(self,name):
-        self.name = name
+### כנסתי להערה בגלל שיוצר קונפליקט עם המחלקה של  המדף צריך לברר למה
+# class Library (Shelve):
+#     def __int__(self,name):
+#         self.name = name
 
 # מחלקה שלוקחת נתונים ממחלקה נמוכה יותר
-class Shelf:
-    def __int__(self,shelf_number):
-        self.shelf_number = shelf_number
-        self.books_count = 0  # משתנה שיספור כמה ספרים יש במדף
-        self.books_pos = 1
-    def UpTo5 (self, NumToAdd, flag):   # add_book(book) זו הפונקציה שניתן להוסיף עד 5 ספרים אני נתתי לה את השם מקסימום 5
-        TotalOfBooks = self.books_count + NumToAdd
+class Shelve:
+    def __int__(self,shelve_number):
+        self.shelve_number = shelve_number
+        self.books = [None] *5 # עשיתי עדכון שיספור , משתנה שיספור כמה ספרים יש במדף
+        self.shalveIsFull = False
 
-    # אם מספר הספרים אחרי ההוספה חורג מהמקסימום - מחזיר את הערך הבוליאני כמו שהוא
-        if TotalOfBooks > 5:
-            return flag
-# אם הערך תקין יחזיר את ההפיך מלא תקין
-        else:
-            return not flag
-
-    def replace_books_pos(self,swap_book):
-        Swap = self.books_pos
-
-        if self.books_pos = 0:
-            return ("swaped")
-        else:
-            return ("position isnt empty")
+    def OneMoreBook (self, book):   # פונקצית הוסמפת ספר
+        for i in range(5):
+            if self.books[i] is None:
+                self.books [i] = book
+                # בודק מצב ספרים על המדף ריק או מלא
+                self.shalveIsFull = all(self.books)
+                return
+                print("Sorry shelve is full")
 
 
-# צריך דחוף המלצה לתיקון הקלאס של המדף כי יש בעיה עם ההחלפות וכמה מקום יש במדף  
+    def replace_books_pos(self,position1,position2):
+       location1 = position1 -1
+       location2 = position2 -1
+       if not (0<= location1 < 5 and 0 <= location2 < 5):
+           print ("bad location  between 1 to 5 only try again ")
+           return
+
+       if self.books[location1] is None or self.books[location2] is None:
+            print("One of the locations is empty cant replace ")
+            return
+
+       self.books[location1] , self.books[location2] = self.books[location2],self.books[location1]
+       print("books swapped positions ")
 
 
-
-
-# הסבר:
-# class BOOK
-# מחלקה שמיצגת ספר ,זו הדרגה הנמוכה ביותר שצריכה להיות חלק גם מכל המחלקות האחרות
 
 class Book:
     def __int__(self,author,title,num_of_pages):
